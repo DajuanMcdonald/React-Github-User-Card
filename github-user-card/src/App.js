d72348230639d7c64ac030e0910151fd185c4971
fiopwk <flopwiki@gmail.com>
2020-02-17 05:08:14 -0500

    mvp showing data to DOM

---
+++ github-user-card/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Cards from './components/Cards'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Github User Card</h1>
+        <Cards/>
+    </div>
+  );
+}
+
+export default App;
